4ea73b384a52a488bf2492e50af361729df682d2
Sohan Rout <sohanrout84@gmail.com>
2025-04-06 22:05:08 +0530

    fixed reigsteration issue

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve HTML pages
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'register.html'));
 });
 
